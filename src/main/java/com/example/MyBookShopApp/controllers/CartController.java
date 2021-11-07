8284b55fc9442288272624c44c8cf622c9b67207
hitYourHeadOnTheKeyboard <midisf@mail.ru>
2021-11-07 15:31:10 +0400

    --some fixes

--- src/main/java/com/example/MyBookShopApp/controllers/CartController.java
+++ src/main/java/com/example/MyBookShopApp/controllers/CartController.java
@@ -1,27 +1,14 @@
 package com.example.MyBookShopApp.controllers;
 
-import com.example.MyBookShopApp.data.Book;
-import com.example.MyBookShopApp.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.util.List;
 
 @Controller
 public class CartController {
 
-    private final BookService bookService;
-
     @Autowired
-    public CartController(BookService bookService) {
-        this.bookService = bookService;
-    }
-
-    @ModelAttribute("cartList")
-    public List<Book> cartBooksList() {
-        return bookService.getBooksData();
+    public CartController() {
     }
 
     @GetMapping("/cart")
